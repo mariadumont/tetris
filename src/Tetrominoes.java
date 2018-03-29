f67ced9839d2163e5518bace087fdc35b6945a33
victor <victor@ti2-15>
2018-03-29 20:01:23 +0200

    First commit

---
+++ src/Tetrominoes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victor
+ */
+public enum Tetrominoes {
+    NoShape, ZShape, SShape, LineShape, TShape, 
+    SquareShape, LShape, MirroredLShape
+}
